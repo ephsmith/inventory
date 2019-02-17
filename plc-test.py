7d65875cb89efd7e1289a9ff0da18d3899b5037e
Forrest Smith <fasmith0229@gmail.com>
2019-02-17 11:04:36 -0600

    updated test script to account for recent modifications

--- plc-test.py
+++ plc-test.py
@@ -1,15 +1,28 @@
-from pycomm.ab_comm.slc import Driver as SlcDriver
 from time import sleep
+import plc
+from plc import Controller
 
-c = SlcDriver()
-if c.open('192.168.200.20'):
-    print('Connected')
-    b3_backup = c.read_tag('B3:0')
-    print('B3:0 = ', b3_backup)
-    print('Writing B3:0 = 256, F8:0 = 2400')
-    c.write_tag('F8:0', 100)
-    c.write_tag('B3:0', 256)
-    print('Done')
-    sleep(10)
-    c.write_tag('B3:0', b3_backup)
-    c.write_tag('F8:0', -2400)
+w = Controller(mode=plc.DRY_RUN)
+
+for c in 'aabbccddeeffgg':
+    w.place(c)
+
+w.print_inventory()
+
+for c in 'abcdefg':
+    w.pick(c)
+
+w.print_inventory()
+
+# c = SlcDriver()
+# if c.open('192.168.200.20'):
+#     print('Connected')
+#     b3_backup = c.read_tag('B3:0')
+#     print('B3:0 = ', b3_backup)
+#     print('Writing B3:0 = 256, F8:0 = 2400')
+#     c.write_tag('F8:0', 100)
+#     c.write_tag('B3:0', 256)
+#     print('Done')
+#     sleep(10)
+#     c.write_tag('B3:0', b3_backup)
+#     c.write_tag('F8:0', -2400)
